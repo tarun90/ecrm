e1f9e609ab0ec1b9a32af75cbee431723f75cd93
kartik <kartik@elsner.com>
2025-02-18 18:12:11 +0530

    updated out-reach to outreach

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -112,7 +112,7 @@ const Sidebar = ({ collapsed, onCollapse }) => {
         },
         {
           key: '10-3',
-          label: 'Out-reach',
+          label: 'Outreach',
           onClick: () => navigate('/outreach/list'),
         },
       ].filter(Boolean),
